b853ae4998fcc7d7c6ddcf9496f7593261a1764f
Declan <declan.l.johnson@gmail.com>
2020-01-14 15:16:53 -0500

    Create vigenere methods

--- crypto-chronology.go
+++ crypto-chronology.go
@@ -37,6 +37,10 @@ func main() {
 
 	scy = scytale_dec(toIntArr("TWETIHAXROESAANSAMNCCNPSIYELPPTAEOHAROSELLFIREYAT"), 5)
 	fmt.Println(toString(scy))
+
+	var vg = vigenere_enc_autokey(toIntArr("WALTERRALEIGHBRINGSTOBACCOTOENGLANDFROMAMERICA"), toIntArr("B")[0])
+	fmt.Println(toString(vg))
+	fmt.Println(toString(vigenere_dec_autokey(vg, toIntArr("B")[0])))
 }
 
 func atbash(text []int) []int {
@@ -60,4 +64,40 @@ func scytale_dec(text []int, lines int) []int {
 		flat = append(flat, content...)
 	}
 	return flat
+}
+
+func vigenere_enc(text []int, key []int) []int {
+	var res []int
+	for pos, char := range text {
+		res = append(res, (char+key[pos])%26)
+	}
+	return res
+}
+
+func vigenere_enc_autokey(text []int, singleKey int) []int {
+	var res []int
+	var key []int
+	key = append(key, singleKey)
+	key = append(key, text[:len(text)-1]...)
+	for pos, char := range text {
+		res = append(res, (char+key[pos])%26)
+	}
+	return res
+}
+
+func vigenere_dec(text []int, key []int) []int {
+	var res []int
+	for pos, char := range text {
+		res = append(res, (char-key[pos]+26)%26)
+	}
+	return res
+}
+
+func vigenere_dec_autokey(text []int, key int) []int {
+	var res []int
+	res = append(res, key)
+	for pos, char := range text {
+		res = append(res, (char-res[pos]+26)%26)
+	}
+	return res
 }
